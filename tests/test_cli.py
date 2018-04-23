b3caf8bc370e029a85b1bafcd3f2fc866240a31f
FisherHuang-Mac <fisherhuang1986@gmail.com>
2018-04-23 23:16:40 +1000

    complete the cli parser test and function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,6 +18,19 @@ def test_parser_with_argument(parser):
     """
     No error is raised if a path is given as an argument.
     """
-    args = parser.parse_args("path/to/inventory.json")
+    args = parser.parse_args(["--export", "path/to/inventory.json"])
     assert args.destination == "path/to/inventory.json"
-    
+
+
+def test_parser_with_flagvalue(parser):
+
+    """
+    The export value is set to True if the --export flag is given
+    """
+    args = parser.parse_args(["--export", "path/to/inventory.json"])
+    assert args.export == True
+    """
+    The export value is default to False if the --export flag is abscent
+    """
+    args = parser.parse_args(["path/to/inventory.json"])
+    assert args.export == False
